91dd86d3625b0e329638fa611c5076fe9985d4df
Emiliano <emikokic@gmail.com>
2016-11-25 12:11:50 -0300

    Mejoras

--- app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
+++ app/src/main/java/ar/edu/unc/famaf/redditreader/ui/PostAdapter.java
@@ -17,6 +17,7 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
 
 import ar.edu.unc.famaf.redditreader.R;
@@ -26,6 +27,8 @@ import ar.edu.unc.famaf.redditreader.model.PostModel;
 public class PostAdapter extends ArrayAdapter<PostModel> {
 
     private List<PostModel> postList = null;
+    private HashMap<String, Bitmap> hashMap = new HashMap<>();
+
 
     public PostAdapter(Context context, int textViewResourseId, List<PostModel> postList) {
         super(context, textViewResourseId);
@@ -69,7 +72,7 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         final ViewHolder viewHolder;
 
@@ -86,42 +89,68 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
             TextView dateTV = (TextView) convertView.findViewById(R.id.dateTV);
             ImageView asyncPreviewIV = (ImageView) convertView.findViewById(R.id.asyncPreviewIV);
             ProgressBar progressBar = (ProgressBar) convertView.findViewById(R.id.progressBar);
-            viewHolder = new ViewHolder(titleTV, authorTV, commentsTV, dateTV, asyncPreviewIV, progressBar);
+            viewHolder = new ViewHolder(titleTV, authorTV, commentsTV, dateTV, asyncPreviewIV, progressBar, position);
             convertView.setTag(viewHolder);
 
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
         }
 
-        PostModel pm = postList.get(position);
+        final PostModel pm = postList.get(position);
 
         viewHolder.titleTV.setText(pm.getTitle());
         viewHolder.authorTV.setText(pm.getAuthor());
         viewHolder.commentsTV.setText(String.valueOf(pm.getComments()));
         viewHolder.dateTV.setText(pm.getDate());
 
-        URL url = null;
-        try {
-            url = new URL(pm.getUrlString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        URL[] urlArray = new URL[1];
-        urlArray[0] = url;
-        new DownloadPreviewImageAsyncTask(){
-            @Override
-            protected void onPostExecute(Bitmap bitmap) {
-                super.onPostExecute(bitmap);
+        // Traigo el bitmap del hashmap, sino esta almacenado el get retorna null
+        Bitmap bitmap = hashMap.get(pm.getUrlString());
+
+        if(bitmap != null) {
+            viewHolder.asyncPreviewIV.setImageBitmap(bitmap);
+            viewHolder.progressBar.setVisibility(ProgressBar.GONE);
+
+        } else {  // Caso en que no tengo almacenada la imagen y debo descargarla
+
+            URL url = null;
+            try {
+                url = new URL(pm.getUrlString());
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                //hashMap.put(pm.getUrlString(), BitmapFactory.decodeResource(getContext().getResources(), R.drawable.reddit_icon));
                 viewHolder.progressBar.setVisibility(ProgressBar.GONE);
-                if (bitmap != null) {
-                    viewHolder.asyncPreviewIV.setImageBitmap(bitmap);
-                } else {
-                    viewHolder.asyncPreviewIV.setImageResource(R.drawable.reddit_icon);
-                }
-
+                viewHolder.asyncPreviewIV.setImageResource(R.drawable.reddit_icon);
             }
-        }.execute(urlArray);
+            URL[] urlArray = new URL[1];
+            urlArray[0] = url;
 
+            // Chequeo que no se haya puesto la imagen por defecto y que no se este descargando ya la imagen
+            if (urlArray[0] != null && !viewHolder.isDownloading)
+
+                viewHolder.isDownloading = true;
+
+                new DownloadPreviewImageAsyncTask() {
+                    @Override
+                    protected void onPostExecute(Bitmap bitmap) {
+                        super.onPostExecute(bitmap);
+                        viewHolder.progressBar.setVisibility(ProgressBar.GONE);
+
+                        if (viewHolder.pos == position) {
+                            if (bitmap != null) {
+                                viewHolder.asyncPreviewIV.setImageBitmap(bitmap);
+                                hashMap.put(pm.getUrlString(), bitmap);
+                            } else {
+                                viewHolder.asyncPreviewIV.setImageResource(R.drawable.reddit_icon);
+                                //hashMap.put(pm.getUrlString(), BitmapFactory.decodeResource(getContext().getResources(), R.drawable.reddit_icon));
+                            }
+                        } else {
+                            viewHolder.asyncPreviewIV.setImageBitmap(bitmap);
+                        }
+
+                        viewHolder.isDownloading = false;
+                    }
+                }.execute(urlArray);
+        }
         return convertView;
     }
 
@@ -132,9 +161,12 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
         public final TextView dateTV;
         public final ImageView asyncPreviewIV;
         public final ProgressBar progressBar;
+        boolean isDownloading;
+        public int pos; // Posicion del elemento en el ViewHolder
+
 
         public ViewHolder(TextView titleTV, TextView authorTV, TextView commentsTV,
-                          TextView dateTV, ImageView asyncPreviewIV, ProgressBar progressBar) {
+                          TextView dateTV, ImageView asyncPreviewIV, ProgressBar progressBar, int pos) {
 
             this.titleTV = titleTV;
             this.authorTV = authorTV;
@@ -142,6 +174,7 @@ public class PostAdapter extends ArrayAdapter<PostModel> {
             this.dateTV = dateTV;
             this.asyncPreviewIV = asyncPreviewIV;
             this.progressBar = progressBar;
+            this.pos = pos;
         }
 
     }
